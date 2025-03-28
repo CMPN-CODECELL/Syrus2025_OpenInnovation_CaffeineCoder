9a75e6d8462071790f3cf959eb523498408ef2ee
Prathamesh <prathameshcode58@gmail.com>
2025-03-28 11:10:55 +0530

    route to resume and controller

--- Backend/index.js
+++ Backend/index.js
@@ -2,21 +2,25 @@ import express from 'express';
 import connectDB from './db/db.js';
 import dotenv from 'dotenv';
 import { userRouter } from './route/user.route.js';
+import resumeRoutes from './route/resume.route.js';
+
+// Config
 dotenv.config();
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-try {
-    connectDB();
-    app.use('/user',userRouter);
-    app.get('/', (req, res) => {
-        res.send('Hello World!');
-    });
+
+connectDB();
+
+app.use('/user',userRouter);
+app.use('/resume', resumeRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
     
-    app.listen(3000, () => {
-        console.log('Server is running on port 3000');
-    });
-} catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-}
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
---
+++ Backend/route/resume.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+const router = express.Router();
+import * from "../controller/resume.controller.js";
+
+router.route("/generate-resume").post(generateResume);
+
+// router.route
+
+export default router;
