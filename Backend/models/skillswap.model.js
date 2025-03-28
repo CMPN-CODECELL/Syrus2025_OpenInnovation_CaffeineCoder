0fa1dbf6ff8d115f95f53588586b87e9d376c111
Raghav-2104 <mundhararaghav16@gmail.com>
2025-03-28 17:41:20 +0530

    Start SkillSwap Session Route

--- Backend/models/skillswap.model.js
+++ Backend/models/skillswap.model.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const skillSwapSchema = new mongoose.Schema({
     userA: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     userB: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    skillOfferedByA: { type: String, required: true },
-    skillOfferedByB: { type: String, required: true },
+    skillOfferedByA: { type: [String], required: true },
+    skillOfferedByB: { type: [String], required: true },
     status: { type: String, enum: ["Pending", "Accepted", "Completed", "Cancelled"], default: "Pending" },
     tokensUsed: { type: Number, required: true }
 }, { timestamps: true });
