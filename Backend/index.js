8fff0ba4164d85eb913c759d2540cf823634e07f
Raghav-2104 <mundhararaghav16@gmail.com>
2025-03-28 09:26:24 +0530

    Connect DB

---
+++ Backend/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import connectDB from './db/db.js';
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+connectDB();
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
